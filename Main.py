67fe8508d97556e695a18482930a28765b23299f
BR1005978 <1005978@hr.nl>
2022-05-20 16:23:16 +0200

    push vanaf een mijn eigen branch. hopelijk werkt dit

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 import sqlite3
 import Menus.HomeScreen as hs
 
+#hallo? sourcetree?
+
+
+# sourcetree aanpassing maar dan vanaf een andere branch
+# ik hoop echt niet dat dit hele project dan ineens in de stront loopt
 
 def Main():
     
