3833ab4e34a473ca1583f339014ad02e671e2cb0
BR1005978 <1005978@hr.nl>
2022-05-20 10:08:23 +0200

    nieuwe folder gemaakt voor menus

---
+++ menus/HomeScreen.py
@@ -0,0 +1,30 @@
+import Login as Login
+
+def homeScreen():
+    '''intro screen '''
+    print(
+        """    ______                     _                   
+   / ____/__  __ _____ ____   (_)_____ ____   _____
+  / /_   / / / // ___// __ \ / // ___// __ \ / ___/
+ / __/  / /_/ // /   / / / // // /__ / /_/ // /    
+/_/     \__,_//_/   /_/ /_//_/ \___/ \____//_/     
+""")
+    print("Welcome to Furnicor Family System v0.01")
+
+    while True:
+        print(
+            """
+1. Login\n
+2. Exit
+            """)
+
+        answer = input("input: ")
+
+        if answer =="1":
+            Login.loginScreen()
+        elif answer =="2":
+            break
+        else:
+            print("Input nog recognized")
+
+homeScreen()
