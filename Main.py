12c09c304183ceb1f9df51219d41d19aaa109509
BR1005978 <1005978@hr.nl>
2022-05-20 17:03:17 +0200

    UwU

--- Main.py
+++ Main.py
@@ -1,11 +1,6 @@
 import sqlite3
 import Menus.HomeScreen as hs
 
-#hallo? sourcetree?
-
-
-# sourcetree aanpassing maar dan vanaf een andere branch
-# ik hoop echt niet dat dit hele project dan ineens in de stront loopt
 
 def Main():
     
